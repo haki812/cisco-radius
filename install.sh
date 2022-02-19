b7f04c9ab4ce8ed125dc7965b3ee6d9b2b36c6c4
haki812 <100011275+haki812@users.noreply.github.com>
2022-02-18 20:50:21 -0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+#复制下面的所有代码，粘贴到你的服务器进行安装，你的服务器必须是Centos7
+#Copy all the code below, paste it into your server to install, your server must be Centos7
+wget https://raw.githubusercontent.com/chendong12/ocserv/master/install_script.sh 
+chmod +x install_script.sh 
+./install_script.sh
+wget https://raw.githubusercontent.com/chendong12/ocserv/master/radius_for_ocserv.sh
+chmod +x radius_for_ocserv.sh
+./radius_for_ocserv.sh
+#
